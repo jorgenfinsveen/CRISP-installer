f8046cb71680188e47cd61e35d0601559f7f793d
Jørgen Finsveen <joergen.finsveen@gmail.com>
2025-10-21 17:29:26 +0200

    Create install_simulator.sh

---
+++ install_simulator.sh
@@ -0,0 +1,19 @@
+cd $HOME
+mkdir -p crisp
+cd crisp
+git clone https://github.com/JRPan/crisp-artifact.git crisp-framework
+cd crisp-framework
+wget https://github.com/jorgenfinsveen/CRISP-installer/blob/main/local_export.sh
+source local_export.sh
+
+source $ROOT/vulkan-sim/setup_environment
+cd $ROOT/mesa-vulkan-sim
+rm -rf build
+meson setup --prefix="${PWD}/lib" build -Dgallium-drivers=iris,swrast,zink -Dvulkan-drivers=intel,amd,swrast -Dplatforms=x11,wayland
+meson configure build
+ninja -C build install
+
+cd $ROOT/vulkan-sim/
+make -j
+cd $ROOT/mesa-vulkan-sim
+ninja -C build/ install
